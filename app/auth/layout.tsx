08959c4e9b2487c34e7724d74de28d4bae8159b2
Euael <euaelmeko@gmail.com>
2025-03-28 13:36:32 +0300

    chore: setup protected layout

--- app/auth/layout.tsx
+++ app/auth/layout.tsx
@@ -1,5 +1,12 @@
+import getServerSession from "@/lib/auth/auth-provider";
+import { redirect } from "next/navigation";
 
-export default function AuthLayout({ children }: Readonly<{ children: React.ReactNode }>) {
+export default async function AuthLayout({ children }: Readonly<{ children: React.ReactNode }>) {
+    const session = await getServerSession();
+    if (session?.user) {
+        redirect("/dashboard")
+    }
+
     return (
         <div className="flex flex-col items-center justify-center min-h-screen p-4">
             {children}
